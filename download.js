a0127ebf8972ac211beccc0e916cc0ae345ba328
Dishendra <dishendramishra@gmail.com>
2019-09-23 16:45:35 +0530

    removed

--- download.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName("btn btn-success")[0].click()
-a = document.getElementsByClassName("btn btn-success btn-file")
-
-setTimeout(function(){
-    a[a.length-1].click()
-    alert("executed");
-},500)
