3e7190edec4afb7a2e6a81799a4e0eabe95a4a99
Dishendra <dishendramishra@gmail.com>
2019-09-23 16:45:28 +0530

    removed

--- script.js
+++
@@ -1,8 +0,0 @@
-function download_page(){
-    key = window.location.href
-    key = key.slice(key.search("v=")+2)
-    window.open("https://y2mate.com/youtube/"+key)
-}
-
-download_page()
-document.getElementsByClassName("btn btn-success")[0].click()
